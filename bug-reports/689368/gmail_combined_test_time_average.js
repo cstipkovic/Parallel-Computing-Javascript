4a82e31561791396c56a45ee95e2b52b3f313fd1
Clauber Stipkovic <clauber.halic@gmail.com>
2015-04-24 20:45:18 -0300

    partial research report and scientific paper creation

--- bug-reports/689368/gmail_combined_test_time_average.js
+++ bug-reports/689368/gmail_combined_test_time_average.js
@@ -1,8 +1,17 @@
-var path_compiled = "/Users/cstipkovic/Documents/Projects/github/cstipkovic/Parallel-Computing-Javascript/bug-reports/689368/parse-tests/gmail_combined.js";
+var path_compiled = "/Users/cstipkovic/Documents/Projects/github/cstipkovic/Parallel-Computing-Javascript/bug-reports/689368/parse-tests/gmail_combined.js",
+    testHits = 100,
+    total = 0;
 
-start = new Date();
-compile(snarf(path_compiled));
-end = new Date();
-print(end - start);
-gc();
+function gmail_combined () {
+    for (i =0; i < testHits; i++) {
+        start = new Date();
+        compile(snarf(path_compiled));
+        end = new Date();
+        print(end - start);
+        total += end - start;
+    }
 
+    print('\nAverage: ' + total / testHits);
+}
+gmail_combined();
+// dis(gmail_combined);
